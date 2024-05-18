5c3fb31a9ba3d4adf1859b835d2a123c7468eb8e
Rebeca Albuquerque <rebeca.albuquerque96@gmail.com>
2024-05-18 17:09:43 -0300

    Setting up for publishing to JitPack

--- fake-server-lib/build.gradle.kts
+++ fake-server-lib/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     id("com.android.library")
     id("kotlin-android")
+    id("maven-publish")
 }
+
+group = "com.github.rebecaalbuquerque"
+version = "1.0.0"
 
 android {
     namespace = "com.albuquerque.fakeserver.lib"
@@ -35,4 +39,35 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                artifactId = project.name
+
+                artifact("${layout.buildDirectory}/outputs/aar/${project.name}-release.aar")
+
+                pom {
+                    name.set("FakeServer")
+                    description.set("A fake server library for Android development")
+                    url.set("https://github.com/rebecaalbuquerque/android-fake-server")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                url = uri("https://jitpack.io")
+            }
+        }
+    }
 }
